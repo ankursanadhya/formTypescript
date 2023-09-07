715f9c4e5c1e2f2dec9fc4389a99b72226583331
ankursanadhya <70885774+ankursanadhya@users.noreply.github.com>
2023-09-07 23:08:32 +0530

    Add files via upload

---
+++ src/store/userSlice.ts
@@ -0,0 +1,35 @@
+// src/store/userSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import type RootState from "../store"
+import { RootState } from './index';
+interface User {
+  name: string;
+  email: string;
+  dob: string;
+  city: string;
+  pincode: string;
+}
+
+interface UserState {
+  users: User[];
+}
+
+const initialState: UserState = {
+  users: [],
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addUser: (state, action: PayloadAction<User>) => {
+      state.users.push(action.payload);
+    },
+  },
+});
+
+export const { addUser } = userSlice.actions;
+
+export const selectUsers = (state: RootState) => state.user.users;
+
+export default userSlice.reducer;
